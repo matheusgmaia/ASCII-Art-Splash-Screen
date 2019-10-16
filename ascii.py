9977ab5f64f8a40b5785fc609812477472f89ca6
matheus.maia <matheus.maia@ccc.ufcg.edu.br>
2019-10-16 01:08:33 -0300

    request

--- ascii.py
+++ ascii.py
@@ -1,5 +1,8 @@
 import random
-import os
+import requests
 i = random.randrange(9) + 1 
 website="https://raw.githubusercontent.com/matheusgmaia/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
-os.system("curl " +  website)
+try:
+    print(requests.get(website, timeout=2).text)
+except:
+    print("Ta sem internet. Ps: Te amo amo")
